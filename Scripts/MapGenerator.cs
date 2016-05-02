621c4fc3f26b6615a5b3e848f1e1661e1817f454
David Long <longda@gmail.com>
2016-05-01 18:45:42 -0700

    episode 4 - colors

--- Scripts/MapGenerator.cs
+++ Scripts/MapGenerator.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour 
 {
+	public enum DrawMode { NoiseMap, ColorMap }
+	public DrawMode drawMode;
+	
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
@@ -17,12 +20,38 @@ public class MapGenerator : MonoBehaviour
 	
 	public bool autoGenerate;
 	
+	public TerrainType[] regions;
+	
 	public void GenerateMap()
 	{
 		var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 		var display = FindObjectOfType<MapDisplay>();
+		var colorMap = new Color[mapWidth * mapHeight];
 		
-		display.DrawNoiseMap(noiseMap);
+		for (var x = 0; x < mapWidth; x++)
+		{
+			for (var y = 0; y < mapHeight; y++)
+			{
+				var currentHeight = noiseMap[x, y];
+				for (var i = 0; i < regions.Length; i++)
+				{
+					if (currentHeight <= regions[i].height)
+					{
+						colorMap[y * mapWidth + x] = regions[i].color;
+						break;
+					}
+				}
+			}
+		}
+		
+		if (drawMode == DrawMode.NoiseMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));	
+		}
+		else if (drawMode == DrawMode.ColorMap)
+		{
+			display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+		}
 	}
 	
 	void OnValidate()
@@ -32,4 +61,12 @@ public class MapGenerator : MonoBehaviour
 		if (lacunarity < 1) lacunarity = 1;
 		if (octaves < 0) octaves = 0;
 	}
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+	public string name;
+	public float height;
+	public Color color;
 }
